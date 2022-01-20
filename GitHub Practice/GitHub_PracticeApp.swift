d4a0eca088362d82fdca06ffef02d7abede72024
Ben Bearup <ben.bearup@yahoo.com>
2022-01-20 14:17:39 -0500

    Initial Commit

---
+++ GitHub Practice/GitHub_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_PracticeApp.swift
+//  GitHub Practice
+//
+//  Created by Benjamin Bearup on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
